f1028b511c0edaa3a147cae199d378fd41f16887
Gavin <oiamo123@gmail.com>
2024-08-09 12:19:57 -0600

    Removed projects

--- NET ASP Core MVC/FutureValueMVC/FutureValueMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FutureValueMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- NET ASP Core MVC/MovieMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MovieMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
